ce63f81f5bff1b64dbd1396edc098e821bf6a70e
ZukoTetyana <Zuko.Tetyana@techyana.co.za>
2025-09-14 10:37:46 +0200

    Initialize StrippedPartsModule with imports and providers

--- backend/src/stripped-parts/stripped-parts.module.ts
+++ backend/src/stripped-parts/stripped-parts.module.ts
@@ -1,1 +1,13 @@
-{}
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { StrippedPart } from './stripped-part.entity';
+import { StrippedPartsService } from './stripped-parts.service';
+import { StrippedPartsController } from './stripped-parts.controller';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([StrippedPart])],
+  providers: [StrippedPartsService],
+  controllers: [StrippedPartsController],
+  exports: [StrippedPartsService],
+})
+export class StrippedPartsModule {}
