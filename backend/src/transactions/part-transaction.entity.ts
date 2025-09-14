823608a7f68adccdb19d56991355c4c37b85b295
ZukoTetyana <Zuko.Tetyana@techyana.co.za>
2025-09-14 10:54:05 +0200

    Define PartTransaction entity with relationships

--- backend/src/transactions/part-transaction.entity.ts
+++ backend/src/transactions/part-transaction.entity.ts
@@ -1,1 +1,25 @@
-{}
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn } from 'typeorm';
+import { Part } from '../entities/part.entity';
+import { User } from '../entities/user.entity';
+import { PartTransactionType } from './transaction-type.enum';
+
+@Entity('part_transactions')
+export class PartTransaction {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => Part, { nullable: false })
+  part: Part;
+
+  @ManyToOne(() => User, { nullable: false })
+  user: User;
+
+  @Column({ type: 'enum', enum: PartTransactionType })
+  type: PartTransactionType;
+
+  @Column({ type: 'int', default: 0 })
+  quantityDelta: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
