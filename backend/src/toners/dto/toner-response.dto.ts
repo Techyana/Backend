2a092b966913f87b21d34254cdca4272a281ac52
ZukoTetyana <Zuko.Tetyana@techyana.co.za>
2025-09-14 10:51:40 +0200

    Define TonerResponseDto with necessary properties
    Added properties to TonerResponseDto for better API documentation.

--- backend/src/toners/dto/toner-response.dto.ts
+++ backend/src/toners/dto/toner-response.dto.ts
@@ -1,1 +1,25 @@
-{}
+import { ApiProperty } from '@nestjs/swagger';
+import { TonerColor } from '../toner-color.enum';
+
+export class TonerResponseDto {
+  @ApiProperty({ type: String, format: 'uuid' })
+  id: string;
+
+  @ApiProperty()
+  model: string;
+
+  @ApiProperty()
+  edpCode: string;
+
+  @ApiProperty({ enum: TonerColor })
+  color: TonerColor;
+
+  @ApiProperty({ type: Number })
+  yield: number;
+
+  @ApiProperty({ type: Number })
+  stock: number;
+
+  @ApiProperty({ type: [String] })
+  forDeviceModels: string[];
+}
