7b3506154b46bd37b99a463861a9ebcb1569629b
ZukoTetyana <Zuko.Tetyana@techyana.co.za>
2025-09-14 09:55:34 +0200

    Initialize NotificationsModule with dependencies
    Notifications Module

--- backend/src/notifications/notifications.module.ts
+++ backend/src/notifications/notifications.module.ts
@@ -1,1 +1,13 @@
-{}
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Notification } from './notification.entity';
+import { NotificationsController } from './notifications.controller';
+import { NotificationService } from './notifications.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Notification])],
+  controllers: [NotificationsController],
+  providers: [NotificationService],
+  exports: [NotificationService],
+})
+export class NotificationsModule {}
