431c4136713a4a0462506eaeaa519337865d4212
ZukoTetyana <Zuko.Tetyana@techyana.co.za>
2025-09-14 12:10:57 +0200

    Initialize ActivityLogsModule with necessary imports

--- backend/src/activity-logs/activity-logs.module.ts
+++ backend/src/activity-logs/activity-logs.module.ts
@@ -1,1 +1,14 @@
-{}
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ActivityLog } from './activity-log.entity';
+import { ActivityLogsController } from './activity-logs.controller';
+import { ActivityLogsService } from './activity-logs.service';
+import { User } from '../users/user.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([ActivityLog, User])],
+  controllers: [ActivityLogsController],
+  providers: [ActivityLogsService],
+  exports: [ActivityLogsService],
+})
+export class ActivityLogsModule {}
