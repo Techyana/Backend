e10af5cac1b4135d1645420e393c34f93a651549
ZukoTetyana <Zuko.Tetyana@techyana.co.za>
2025-09-14 18:07:46 +0200

    Add UserResponseDto with properties and Swagger decorators

--- backend/src/users/dto/user-response.dto.ts
+++ backend/src/users/dto/user-response.dto.ts
@@ -1,1 +1,25 @@
-{}
+import { ApiProperty } from '@nestjs/swagger';
+import { Role } from '../role.enum';
+
+export class UserResponseDto {
+  @ApiProperty({ type: 'string', format: 'uuid' })
+  id: string;
+
+  @ApiProperty({ type: 'string' })
+  name: string;
+
+  @ApiProperty({ type: 'string' })
+  surname: string;
+
+  @ApiProperty({ type: 'string', format: 'email' })
+  email: string;
+
+  @ApiProperty({ type: 'string' })
+  rzaNumber: string;
+
+  @ApiProperty({ enum: Role })
+  role: Role;
+
+  @ApiProperty({ type: 'boolean', required: false })
+  mustChangePassword?: boolean;
+}
