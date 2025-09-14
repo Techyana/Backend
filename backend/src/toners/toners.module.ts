6614b687cf9b99ee56879c73904b0f194190d515
ZukoTetyana <Zuko.Tetyana@techyana.co.za>
2025-09-14 10:47:37 +0200

    Initialize TonersModule with TypeORM integration

--- backend/src/toners/toners.module.ts
+++ backend/src/toners/toners.module.ts
@@ -1,1 +1,13 @@
-{}
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Toner } from './toner.entity';
+import { TonersService } from './toners.service';
+import { TonersController } from './toners.controller';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Toner])],
+  providers: [TonersService],
+  controllers: [TonersController],
+  exports: [TonersService],
+})
+export class TonersModule {}
