b2953b794c04600e4997a2134879ced00234fea3
ZukoTetyana <Zuko.Tetyana@techyana.co.za>
2025-09-14 18:19:11 +0200

    Update transactions.module.ts

--- backend/src/transactions/transactions.module.ts
+++ backend/src/transactions/transactions.module.ts
@@ -1,1 +1,15 @@
-{}
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PartTransaction } from './part-transaction.entity';
+import { TransactionsController } from './transactions.controller';
+import { TransactionsService } from './transactions.service';
+import { Part } from '../parts/part.entity';
+import { User } from '../users/user.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([PartTransaction, Part, User])],
+  controllers: [TransactionsController],
+  providers: [TransactionsService],
+  exports: [TransactionsService],
+})
+export class TransactionsModule {}
