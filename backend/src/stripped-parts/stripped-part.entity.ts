dd27747fb98e2fe8f17269b15d4646b1a5420ed7
ZukoTetyana <Zuko.Tetyana@techyana.co.za>
2025-09-14 10:35:00 +0200

    Define StrippedPart entity with relationships and fields

--- backend/src/stripped-parts/stripped-part.entity.ts
+++ backend/src/stripped-parts/stripped-part.entity.ts
@@ -1,1 +1,21 @@
-{}
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn } from 'typeorm';
+import { Part } from '../entities/part.entity';
+import { Device } from '../entities/device.entity';
+
+@Entity('stripped_parts')
+export class StrippedPart {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => Part, { nullable: false })
+  part: Part;
+
+  @ManyToOne(() => Device, { nullable: false })
+  device: Device;
+
+  @Column({ type: 'varchar', length: 255 })
+  partName: string;
+
+  @CreateDateColumn()
+  strippedAt: Date;
+}
