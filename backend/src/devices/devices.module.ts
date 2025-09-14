461119110439fb99218a1ac4c884ac480bb4cc74
ZukoTetyana <Zuko.Tetyana@techyana.co.za>
2025-09-14 09:38:08 +0200

    Initialize DevicesModule with TypeOrm and controllers
    Devices Module

--- backend/src/devices/devices.module.ts
+++ backend/src/devices/devices.module.ts
@@ -1,1 +1,16 @@
-{}
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { DevicesController } from './devices.controller'
+import { DevicesService } from './devices.service'
+import { Device } from './device.entity'
+import { StrippedPart } from '../stripped-parts/stripped-part.entity'
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Device, StrippedPart]),
+  ],
+  controllers: [DevicesController],
+  providers: [DevicesService],
+  exports: [DevicesService],
+})
+export class DevicesModule {}
